b6805efca9be09df967e19a04a4497f6838eea34
App Academy Student <student@appacademy.io>
2016-09-16 14:54:24 -0700

    node knows children

--- tic-tac-toe/lib/tic_tac_toe_node.rb
+++ tic-tac-toe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,32 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    # new_node = node( next_board, next_mark, pos)
+    child_states = []
+
+    empty_positions(@board).each do |pos|
+      duped_board = @board.dup
+      duped_board[pos] = @next_mover_mark
+      child_states << TicTacToeNode.new(duped_board, opposite_mark, pos)
+    end
+
+    child_states
+  end
+
+  def opposite_mark
+    @next_mover_mark == :x ? :o : :x
+  end
+
+  def empty_positions(board)
+    positions = []
+
+    (0..2).each do |x|
+      (0..2).each do |y|
+        new_pos = [x,y]
+        positions << new_pos if board.empty?(new_pos)
+      end
+    end
+
+    positions
   end
 end
