a0d3e1b3b1ce980dccdb023efb1e510acb41c8a9
App Academy Student <student@appacademy.io>
2016-09-16 11:08:54 -0700

    implement node structure
    * implemented parent, children, value for node
    * made parent setter method add node to parent's children unless it's there already

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,30 @@
 class PolyTreeNode
+  attr_accessor :parent, :children, :value
+
+  def initialize(value)
+    @children = []
+    @parent = nil
+    @value = value
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(new_parent)
+    @parent = new_parent
+    unless new_parent.nil? || new_parent.children.include?(self)
+      new_parent.children << self
+    end
+  end
+
 
 end
