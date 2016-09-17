104e4f2bd09835a156c9a748372502aa62b6a9fd
Todd Nestor <todd.nestor@gmail.com>
2016-09-17 00:40:55 -0700

    kpf only builds tree until the end

--- knight-path-finder/lib/knight-path-finder.rb
+++ knight-path-finder/lib/knight-path-finder.rb
@@ -6,7 +6,7 @@ class KnightPathFinder
     raise unless on_board?(pos)
     @root = PolyTreeNode.new(pos)
     @visited_positions = [pos]
-    build_move_tree
+    # build_move_tree
   end
 
   MOVES = [
@@ -52,12 +52,27 @@ class KnightPathFinder
   def find_path(end_pos)
     raise unless on_board?(end_pos)
 
-    node = @root.bfs(end_pos)
+    queue = [@root]
+    found_node = nil
+
+    until queue.empty? || found_node
+      next_item = queue.shift
+      if next_item.value == end_pos
+        found_node = next_item
+      else
+        valid_moves(next_item.value).each do |move|
+          node = PolyTreeNode.new(move)
+          node.parent = next_item
+          queue << node
+        end
+      end
+    end
+
     moves = []
 
-    until node.nil?
-      moves << node.value
-      node = node.parent
+    until found_node.nil?
+      moves << found_node.value
+      found_node = found_node.parent
     end
 
     moves.reverse
